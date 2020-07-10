7d4ee73b291f937eac672175f9b54363c64ac571
Ryan Slater <ryan.j.slater.2@gmail.com>
2020-07-10 09:15:38 -0400

    Still didn't work, trying something new

--- cogs/invites.py
+++ cogs/invites.py
@@ -58,7 +58,7 @@ class Invites(commands.Cog):
                     await log(f'{purpose} {member} has joined in {channelName} and was given role {roleName}')
 
                     # If new member is a prospective student, automatically say hello
-                    await channel = self.bot.fetch_channel(702895094881058896)
+                    channel = self.bot.get_channel(702895094881058896)
                     print(f'CHANNEL: {channel}')
                     await channel.send(f'Hello, {member.mention}')
 
