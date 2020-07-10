9d82a295005a630c3840e103f913425b8f8b89f5
Ryan Slater <ryan.j.slater.2@gmail.com>
2020-07-10 10:27:27 -0400

    Added template for support ticket command

---
+++ cogs/supporttickets.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+
+
+class Template(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    ###### ================================== ######
+    ######              Commands              ######
+    ###### ================================== ######
+
+    @commands.command()
+    async def supportticket(self, ctx):
+        await ctx.send(f'This is a feature currently being developed. For now, if you have a question for CSE Support, @ them or email them at cse-support.wright.edu')
+
+
+def setup(bot):
+    bot.add_cog(Template(bot))
+
+
+async def log(string, timestamp=True):
+    if timestamp:
+        print(f'[{str(datetime.now())[:-7]}]', end=' ')
+    print(string)
