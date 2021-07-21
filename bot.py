fe649d33b8d1baa4e265c7b758e964b39cc0aab9
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-07-21 12:46:57 -0400

    Renamed client to bot in bot.py

--- bot.py
+++ bot.py
@@ -10,68 +10,68 @@ from dotenv import load_dotenv
 from utils import *
 
 intents = discord.Intents(messages=True, guilds=True, members=True, voice_states=True)
-client = commands.Bot(command_prefix='-', intents=intents)
+bot = commands.Bot(command_prefix='-', intents=intents)
 start_time = time()
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 
-@client.event
+@bot.event
 async def on_ready():
     # Set up Discord Components
-    DiscordComponents(client)
+    DiscordComponents(bot)
 
     # Startup status
-    await client.change_presence(activity=discord.Game('Booting'), status=discord.Status.dnd)
+    await bot.change_presence(activity=discord.Game('Booting'), status=discord.Status.dnd)
 
     # Start logging
-    await log(client, '\n\n\n\n\n', False)
-    await log(client, '###################################')
-    await log(client, '# BOT STARTING FROM FULL SHUTDOWN #')
-    await log(client, '###################################')
+    await log(bot, '\n\n\n\n\n', False)
+    await log(bot, '###################################')
+    await log(bot, '# BOT STARTING FROM FULL SHUTDOWN #')
+    await log(bot, '###################################')
 
     # Load all cogs
-    await client.change_presence(activity=discord.Game(f'Loading Cogs'), status=discord.Status.idle)
+    await bot.change_presence(activity=discord.Game(f'Loading Cogs'), status=discord.Status.idle)
     for file in os.listdir('Cogs'):
         if not file.startswith('__') and file.endswith('.py'):
             try:
-                client.load_extension(f'Cogs.{file[:-3]}')
-                await log(client, f'Loaded cog: {file[:-3]}')
+                bot.load_extension(f'Cogs.{file[:-3]}')
+                await log(bot, f'Loaded cog: {file[:-3]}')
             except commands.errors.NoEntryPointError:
                 pass
 
     # Show the bot as online
-    await client.change_presence(activity=discord.Game('Raider Up!'), status=discord.Status.online, afk=False)
-    await log(client, 'Bot is online')
+    await bot.change_presence(activity=discord.Game('Raider Up!'), status=discord.Status.online, afk=False)
+    await log(bot, 'Bot is online')
 
     # Print startup duration
-    await log(client, '#########################')
-    await log(client, '# BOT STARTUP COMPLETED #')
-    await log(client, '#########################\n')
-    await log(client, f'Started in {round(time() - start_time, 1)} seconds')
+    await log(bot, '#########################')
+    await log(bot, '# BOT STARTUP COMPLETED #')
+    await log(bot, '#########################\n')
+    await log(bot, f'Started in {round(time() - start_time, 1)} seconds')
 
 
-@client.event
+@bot.event
 async def on_command_error(ctx, error):
     author, message = ctx.author, ctx.message.content
 
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Missing required arguement')
         await ctx.send_help()
-        await log(client, f'{author} attempted to run `{message}` but failed because they were missing a required argument')
+        await log(bot, f'{author} attempted to run `{message}` but failed because they were missing a required argument')
 
     elif isinstance(error, commands.MissingRole):
         await ctx.send('Missing role')
-        await log(client, f'{author} attempted to run `{message}` but failed because they were missing a required role')
+        await log(bot, f'{author} attempted to run `{message}` but failed because they were missing a required role')
 
     elif isinstance(error, commands.CommandNotFound):
-        await log(client, f'{author} attempted to run `{message}` but failed because the command was not found')
+        await log(bot, f'{author} attempted to run `{message}` but failed because the command was not found')
 
     else:
         await ctx.send(f'Unexpected error: {error}')
-        await log(client, f'{author} attempted to run `{message}` but failed because of an unexpected error: {error}')
+        await log(bot, f'{author} attempted to run `{message}` but failed because of an unexpected error: {error}')
 
 
 if __name__ == '__main__':
     # Run bot from key given by command line argument
-    client.run(TOKEN)
+    bot.run(TOKEN)
