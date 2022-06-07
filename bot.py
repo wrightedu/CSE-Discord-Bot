f5f9d8d59aaa0ccd04aa3d1f2b276baa838a0863
Anmol Saini <anmolsaini@pop-os.localdomain>
2022-06-07 12:20:09 -0400

    Reverted changes to bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,9 @@ from dotenv import load_dotenv
 from utils import *
 
 intents = discord.Intents(messages=True, guilds=True, members=True, voice_states=True)
-bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+bot = commands.Bot(command_prefix='-', intents=intents)
 start_time = time()
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -50,7 +52,18 @@ async def on_ready():
                 pass
 
     # Show the bot as online
-    await bot.change_presence(activity=discord.Game('Raider Up!'), status=discord.Status.online, afk=False)
+    # If the bot had a status prior to shutting down, restore it
+    # if it didn't, set it to 'Raider Up!'
+
+    try:
+        async with aiofiles.open('status.txt', mode='r') as sf:
+            contents = await sf.read()
+    except FileNotFoundError:
+        async with aiofiles.open('status.txt', mode='w') as sf:
+            await sf.write('Raider Up!')
+            contents = 'Raider Up!'
+
+    await bot.change_presence(activity=discord.Game(contents), status=discord.Status.online, afk=False)
     await log(bot, 'Bot is online')
 
     # Print startup duration
@@ -70,7 +83,7 @@ async def on_command_error(ctx, error):
     author, message = ctx.author, ctx.message.content
 
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Missing required arguement')
+        await ctx.send('Missing required argument')
         await ctx.send_help()
         await log(bot, f'{author} attempted to run `{message}` but failed because they were missing a required argument')
 
