bebadfd0c129eeae155fe0a0fa7dd9e68d9f1444
ReginPotter <77698810+ReginPotter@users.noreply.github.com>
2022-07-05 16:49:01 -0400

    Destroy_server refactor
    We renamed destroy_server to destroy_courses.
    We also refactored destroy_server to make it more straight forward.
    Changed build_classes to build_courses to be in line with new stuff.

--- Cogs/TestServerManagement.py
+++ Cogs/TestServerManagement.py
@@ -6,6 +6,7 @@ import emoji as discord_emoji
 import pandas as pd
 import validators
 from discord.ext import commands
+from discord.utils import get
 # from discord_components import Button, ButtonStyle, InteractionType
 from utils import *
 
@@ -17,10 +18,63 @@ class TestServerManagement(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def get_category(self, ctx, category_names):
+        """ Looks through all categories and verifies if the list of category names is on the server
+
+        Args:
+            category_names (List[str]): a list of strings of the category names to delete
+
+        Outputs:
+            categories (List[categories]): a list of the verified categories.
+        
+        """
+
+        # Get list of all categories (category objects) to be destroyed and print
+        categories = []
+        missing_categories = '__**MISSING FOLLOWING CATEGORIES**__\n'
+
+        for category_name in category_names:
+            category = get(ctx.guild.categories, name=category_name)
+
+            # If the category was not found it adds it to the missing_categories message
+            if category == None:
+                missing_categories += f'{category_name}\n'
+            else:
+                categories.append(category)
+        await ctx.send(missing_categories)
+
+        return categories
+
+    async def get_roles(self, ctx, role_names):
+        """ Looks through all roles and verifies if the list of role names is on the server
+
+        Args:
+            role_names (List[str]): a list of strings of the role names to delete
+
+        Outputs:
+            roles (List[roles]): a list of the verified roles.
+
+        """
+
+        # Get list of all categories (category objects) to be destroyed and print
+        roles = []
+        missing_roles = '__**MISSING FOLLOWING ROLES**__\n'
+
+        for role_name in role_names:
+            role = get(ctx.guild.roles, name=role_name)
+
+            # If the role was not found it adds it to the missing_roles message
+            if role == None:
+                missing_roles += f'{role_name}\n'
+            else:
+                roles.append(role)
+        await ctx.send(missing_roles)
+
+        return roles
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def buildclasses(self, ctx):
+    async def buildcourses(self, ctx):
         """Create class channels
         ETC
         """
@@ -40,15 +94,15 @@ class TestServerManagement(commands.Cog):
             await ctx.message.attachments[0].save(csv_filepath)
         
         # Load roles csv
-        roles_df = pd.read_csv(csv_filepath)
+        courses_df = pd.read_csv(csv_filepath)
 
-        roles_df = roles_df.dropna(subset=['create_channels'])
+        courses_df = courses_df.dropna(subset=['create_channels'])
 
         #? TODO Build all channels
-        role_names=roles_df["role/link"].to_list()
-        class_channels = roles_df["create_channels"].to_list()
-        categories = roles_df["text"].to_list()
-        long_names = roles_df["long_name"].to_list()
+        role_names=courses_df["role/link"].to_list()
+        class_channels = courses_df["create_channels"].to_list()
+        categories = courses_df["text"].to_list()
+        long_names = courses_df["long_name"].to_list()
 
         # Print to user list of channels to build
         message = '__**CREATE FOLLOWING CATEGORIES**__\n'
@@ -92,15 +146,64 @@ class TestServerManagement(commands.Cog):
                         await category.create_voice_channel(channel_name)
                     else:
                         await category.create_voice_channel(channel_name, user_limit=int(member_count))
-
+        await ctx.send('All requested courses have been built!')
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def destroyclasses(self, ctx):
-        """Destroy class channels
+    async def destroycourses(self, ctx):
+        """Destroy course channels
         ETC
         """
 
+        # Load roles csv
+        csv_filepath = f'role_lists/roles_{ctx.guild.id}.csv'
+
+        # If the role_lists directory doesn't exist raise a FileNotFoundError back to the buildserver command
+        try:
+            courses_df = pd.read_csv(csv_filepath)
+        except FileNotFoundError:
+            raise FileNotFoundError
+
+        courses_df = courses_df.dropna(subset=['create_channels'])
+
+
+        # List of names of categories to be destroyed, as determined by saved csv
+        destroy_category_names = courses_df['text'].tolist()
+        destroy_categories = await self.get_category(ctx, destroy_category_names)
+
+        # List of names of role to be destroyed, as determined by saved csv
+        destroy_role_names = courses_df['role/link'].tolist()
+        destroy_roles = await self.get_roles(ctx, destroy_role_names)
+        
+
+        message = '__**DESTROY FOLLOWING CATEGORIES**__\n'
+        if len(destroy_categories):
+            for category in destroy_categories:
+                message += f'{category.name}\n'
+        else:
+            message += f'NO CATEGORIES FOUND\n'
+        
+        message += '__**DESTROY FOLLOWING ROLES**__\n'
+        if len(destroy_roles):
+            for role in destroy_roles:
+                message += f'{role.mention}\n'
+        else:
+            message += f'NO ROLES FOUND\n'
+        
+        await ctx.send(message)
+        if not await confirmation(self.bot, ctx, 'destroy'):
+            return
+        
+        # Destroy categories and all subchannels
+        for category in destroy_categories:
+            for channel in category.channels:
+                await channel.delete()
+            await category.delete()
+        
+        for role in destroy_roles:
+            await role.delete()
+
+        await ctx.send('All requested courses have been destroyed!')
 # assuming they are built together and we can trust that, 
 # can delete roles and categories together, however,
 # when searching categories and roles, they will need to be searched separately
