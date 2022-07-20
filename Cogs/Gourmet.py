46a0ce1b180a12bce27dc15fb0794fafe2b0273a
ReginPotter <77698810+ReginPotter@users.noreply.github.com>
2022-07-20 16:46:39 -0400

    File IO functionality added
    Added File IO
    Added randomizing restaurants
    Added restaurants.txt file in assets
    Added functionality to all the buttons

--- Cogs/Gourmet.py
+++ Cogs/Gourmet.py
@@ -2,6 +2,7 @@ import os
 import sys
 from time import sleep
 import re
+import random
 
 from discord.ui import View, Button
 from discord.ext import commands
@@ -18,20 +19,25 @@ class Gourmet(commands.Cog):
         self.normal_restaurant = []
         self.vegan_restaurant = []
 
-    # Adding items to the normal_restaurant list
-    def meat_add(self, rest_name):
-        self.normal_restaurant.append(rest_name)
-
     # Menu that extends from a View
     class GourmetMenu(View):
         def __init__(self, *, cog, timeout=180):
             super().__init__(timeout=timeout)
             self.cog = cog
-        
+            self.count = 0
+            random.shuffle(cog.normal_restaurant)
+            random.shuffle(cog.vegan_restaurant)
+
+
         # Not working yet
-        @discord.ui.button(label="Meat",style=discord.ButtonStyle.blurple,emoji='\U0001F3B1')
+        @discord.ui.button(label="Random",style=discord.ButtonStyle.blurple,emoji='\U0001F3B1')
         async def roll_M(self,interaction:discord.Interaction,button:discord.ui.Button):
-            await interaction.response.edit_message(view=self)
+            if not len(self.cog.normal_restaurant) == 0:
+                await interaction.response.send_message(self.cog.normal_restaurant.pop())
+                print(self.cog.normal_restaurant)
+                # self.count += 1
+            else:
+                await interaction.response.send_message('You have run out of restaurants. You are going to arbys.')
         
         # Adds a restaurant to a list
         @discord.ui.button(label="Add Restaurant",style=discord.ButtonStyle.green)
@@ -44,7 +50,9 @@ class Gourmet(commands.Cog):
 
             # Waiting for the user's response (with interactions!)
             msg = await interaction.client.wait_for('message', check=lambda message: message.author == interaction.user)
+            msg.content = msg.content.casefold()
             self.cog.normal_restaurant.append(msg.content)
+            print(msg.content)
 
         # Removes a restaurant from a list
         @discord.ui.button(label="Remove Restaurant",style=discord.ButtonStyle.red)
@@ -57,7 +65,9 @@ class Gourmet(commands.Cog):
 
             # Waiting for the user's response (with interacitons!)
             msg = await interaction.client.wait_for('message', check=lambda message: message.author == interaction.user)
+            msg.content = msg.content.casefold()
 
+            print(msg.content)
             # If the restaurant is not in the list it responds with an error
             if not msg.content in self.cog.normal_restaurant:
                 await interaction.message.reply(content=f'Error: {msg.content} is not in the list.')
@@ -67,11 +77,27 @@ class Gourmet(commands.Cog):
         # Not working yet
         @discord.ui.button(label="Vegan",style=discord.ButtonStyle.blurple,emoji='\U0001F96C')
         async def roll_V(self,interaction:discord.Interaction,button:discord.ui.Button):
-            await interaction.response.edit_message(view=self)
+            if not len(self.cog.vegan_restaurant) == 0:
+                await interaction.response.send_message(self.cog.vegan_restaurant.pop())
+                print(self.cog.vegan_restaurant)
+            else:
+                await interaction.response.send_message('You have run out of restaurants. You are going to arbys.')
+            # await interaction.response.edit_message(view=self)
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def feedMe(self, ctx):
+        self.normal_restaurant = []
+        self.vegan_restaurant = []
+        with open('assets/restaurants.txt', 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not self.normal_restaurant:
+                    self.normal_restaurant = line.split(sep=', ')
+                else:
+                    self.vegan_restaurant = line.split(sep=', ')
+        print(self.normal_restaurant)
+        print(self.vegan_restaurant)
         await ctx.send(view=self.GourmetMenu(cog=self))
 
 
