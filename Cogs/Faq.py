4f52cf8bee7b690d458137856216f7516fa5393d
Anmol Saini <anmolsaini@pop-os.localdomain>
2022-07-25 14:58:07 -0400

    Made faq channel persistent
    Loads channels being monitored by faq bot into a list from a file upon startup.
    Overwrites file with channels in list when it is updated.

--- Cogs/Faq.py
+++ Cogs/Faq.py
@@ -1,14 +1,30 @@
+from pathlib import Path
+
 from discord.ext import commands
 from utils import *
 
 async def setup(bot):
-    await bot.add_cog(Faq(bot))
-
+    channels_path = r"assets/FAQ/channels.txt"
+    path = Path(channels_path)
+    channel_names = []
+    
+    # if file exists, appends channel names to a list for use with faq command
+    if path.is_file():
+        with path.open() as f:
+            for channel_name in f:
+                channel_name = channel_name.strip()
+                channel_names.append(channel_name)
+        print(channel_names)
+    # initialize cog with no file
+    else:
+        print("Channels file for faq command does not exist.")
+    
+    await bot.add_cog(Faq(bot, channel_names))
 
 class Faq(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, channel_names):
         self.bot = bot
-        self.faq_channels = []
+        self.channel_names = channel_names
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -20,11 +36,11 @@ class Faq(commands.Cog):
             return
 
         # Checks if the channel is a requested faq channel
-        if message.channel in self.faq_channels:
+        if message.channel.name in self.channel_names:
 
             # If there is a ? in the content reply "That looks interesting" and break out of the loop
             if '?' in message.content:
-                await message.reply("That looks interesting")
+                await message.reply("Your question has been noted. Thank you for helping us test the bot!")
                 # better response and @Wischgoll for advice
     
     @commands.command()
@@ -34,17 +50,23 @@ class Faq(commands.Cog):
         Allows/disallows the faq machine learning feature for a specific channel
         """
         # If the channel is in the list remove it and return
-        if ctx.channel in self.faq_channels:
-            self.faq_channels.remove(ctx.channel)
+        if ctx.channel.name in self.channel_names:
+            self.channel_names.remove(ctx.channel.name)
             await ctx.reply("FAQ has been disabled for this channel!")
 
             # Logging
             await log(self.bot, f'{ctx.author} has disabled FAQ for the {ctx.channel} channel')
-            return
         
         # If the channel is not in the list add it to the end
-        await ctx.reply("FAQ has been enabled for this channel!")
-        self.faq_channels.append(ctx.channel)
+        else:
+            await ctx.reply("FAQ has been enabled for this channel!")
+            self.channel_names.append(ctx.channel.name)
 
-        # Logging
-        await log(self.bot, f'{ctx.author} has enabled FAQ for the {ctx.channel} channel')
+            # Logging
+            await log(self.bot, f'{ctx.author} has enabled FAQ for the {ctx.channel} channel') 
+
+        channels_path = r"assets/FAQ/channels.txt"
+        path = Path(channels_path)
+        with path.open('w') as f:
+            for channel_name in self.channel_names:
+                f.write(f"{channel_name}\n")
