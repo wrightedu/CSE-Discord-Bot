c7bfbe46881eede9b92ef5239f6b0b3a5f418739
Mason McDaniel <kokuko3@pop-os.localdomain>
2022-09-30 10:58:22 -0400

    Commented code

--- utils/checkinmenu.py
+++ utils/checkinmenu.py
@@ -2,20 +2,21 @@ from discord.ui import View
 
 from utils.utils import *
 
-
+"""Button menu for the checkin command. Checkout, Help, and Pomodoro."""
 class checkinmenu(View):
     def __init__(self, *, timeout=180):
         super().__init__(timeout=timeout)
+    #Checkout button that displays a message that the user has finished their tasks.
     @discord.ui.button(label="Checkout", style=discord.ButtonStyle.green, emoji='\U0001F6D2')
     async def checkout(self, interaction:discord.Interaction, button:discord.ui.Button):
         timestamp = datetime.datetime.now().strftime(r"%I:%M %p")
         await interaction.response.send_message(content=f"{interaction.user.display_name} checked out @{timestamp}")
-    
+    #Help button that displays a message that the user needs help with a task.
     @discord.ui.button(label="HELP!", style=discord.ButtonStyle.red,  emoji='\U0001F6D2')
     async def HELP(self, interaction:discord.Interaction, button:discord.ui.Button):
         timestamp = datetime.datetime.now().strftime(r"%I:%M %p")
         await interaction.response.send_message(content=f"{interaction.user.display_name} sent out an SOS @{timestamp}")
-    
+    #Pomodoro button (IN PROGRESS).
     @discord.ui.button(label="Pomodoro", style=discord.ButtonStyle.blurple, emoji='\U0001F6D2')
     async def Pomodoro(self, interaction:discord.Interaction, button:discord.ui.Button):
         timestamp = datetime.datetime.now().strftime(r"%I:%M %p")
