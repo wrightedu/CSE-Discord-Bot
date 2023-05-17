7b1f663ace200a02c2732da006e920b5e5da6f5a
Mason McDaniel <mcdaniel.121@wright.edu>
2023-05-17 13:47:35 -0400

    Adding a check for the csv file

--- Cogs/Checkin.py
+++ Cogs/Checkin.py
@@ -2,7 +2,8 @@ from discord.ui import Button
 from discord.ui import View
 from discord.ext import commands
 from discord import app_commands
-
+from validators import url
+import pandas as pd
 from utils.utils import *
 
 async def setup(bot):
@@ -13,12 +14,30 @@ class Checkin(commands.Cog):
         self.bot = bot
 
     @app_commands.command(description="Add, list, or mark your tasks as complete")
-    async def task(self, interaction:discord.Interaction, message:str):
+    async def task(self, interaction:discord.Interaction, task:str, issue: str = 'None'):
         """A function designed to allow the user to track small tasks
         to increase productivity. 
         
         Outputs: 
             DM task list to user.
         """
+        await interaction.response.send_message("Check your DM's", ephemeral=True)
+        options = [task]
+        if (issue != 'None'):
+            options.append(issue)
         channel = await interaction.user.create_dm()
-        await channel.send('Test')
+        csv_filepath = f'assets/Tasklists/{interaction.user.id}.csv'
+        try:
+            tasks_df = pd.read_csv(csv_filepath)
+        except FileNotFoundError:
+            await interaction.followup.send("No task file found. Please make a .csv file for your tasks", ephemeral=True)
+            raise FileNotFoundError
+        task_numbers = tasks_df["number"].to_list()
+        if (len(task_numbers) != 0):
+            task_num = task_numbers[-1] + 1
+        else:
+            task_num = 1    
+        await channel.send(f'Task: {task}; Num: {task_num}; URL: {issue}')
+
+
+
