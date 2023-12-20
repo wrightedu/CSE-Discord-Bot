715c967c2bdcbdaa15a3e6d738b2db7aa212a9ed
bpennie <bpennie@pop-os.localdomain>
2023-12-20 11:53:20 -0500

    Added listener functionality to listen for help room messages from TAs to react to

--- Cogs/Listeners.py
+++ Cogs/Listeners.py
@@ -19,3 +19,17 @@ class Listeners(commands.Cog):
         if type(ctx.channel) == discord.TextChannel:
             if ctx.channel.name == 'i-made-a-pr' and ctx.author != self.bot.user:
                 await ctx.add_reaction("💩")
+
+            # Get the TA role from its name
+            ta_role_name = 'Teaching Assistant'
+            ta_role = discord.utils.get(ctx.guild.roles, name=ta_role_name)
+            
+            # Only react if its in the help room channel and if the user has the TA role
+            if ctx.channel.name == 'cs-help-room' and ta_role in ctx.author.roles:
+                # Establishes a list of keywords to check for in the message
+                keywords = ['greetings', 'hi', 'hello', 'hey', 'howdy', 'yo', 'sup', 'office hours', 'help room', 'helproom', 'russ', 'joshi']
+                message = ctx.content
+
+                # Add the reaction if any keywords are found in the message
+                if any(keyword in message.lower() for keyword in keywords):
+                    await ctx.add_reaction("👋")
