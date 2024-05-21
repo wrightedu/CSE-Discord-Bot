8a246f8b37029e194ec771c61001a0e8631c667b
bibekrj <x_dev@outlook.com>
2024-05-21 17:03:25 -0400

    Boilerplate for insert user function

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -2,8 +2,6 @@ import os
 import sqlite3
 
 
-
-
 def initialize_db(db_location) -> None:
     """
     Create a new database file in SQLITE3 with Tables. 
@@ -12,7 +10,7 @@ def initialize_db(db_location) -> None:
     Args: 
         db_location(string): Path to where the database file is stored. 
             Usually project directory
-    
+
     Output: 
         An error if raised
     """
@@ -54,7 +52,7 @@ def initialize_db(db_location) -> None:
     pomo_id INTEGER,
     FOREIGN KEY(pomo_id) REFERENCES pomodoro(pomo_id)
     )"""
-    
+
     try:
         cursor.execute(create_user_table_query)
         cursor.execute(create_timesheet_table_query)
@@ -65,4 +63,46 @@ def initialize_db(db_location) -> None:
     except Exception as e:
         print(f'Could not create table because {e} occured')
     conn.close()
-    
+
+
+def create_connection(db_file: str):
+    """
+    Takes the location of the db file and creates a connection to the database
+    Args:
+        db_file (str): The name and path to the database file
+    Outputs:
+        returns an object `conn` that represents the connection the database
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(f'Connected to the Database {db_file}')
+        return conn
+    except sqlite3.Error as e:
+        print(f'Could not connect to database because {e}')
+    return conn
+
+
+def insert_user(conn, discord_id: str, discord_name: str,
+                date_registered: str) -> None:
+    """
+    Takes the arguments to create a new record in the User Table
+    discord_id and date_registered are NOT NULL in the database and must be provided
+
+    Args:
+        discord_id (str): The discord user id of the user
+        discord_name (str): The name of the user
+        date_registered(str): The date and time when the user hits the register button
+    """
+    conn = create_connection(conn)
+    if conn is not None:
+        try:
+            # c = conn.cursor()
+            # c.execute("B")
+            pass
+        except sqlite3.Error as e:
+            print(e)
+            conn.rollback()
+            return None
+    else:
+        print("Error! Cannot create database connection")
