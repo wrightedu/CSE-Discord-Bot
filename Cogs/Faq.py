01e6f97ee1f0c979f2a2296e14d62680d2c7e7c6
Makaela Sellers <sellers.52@wright.edu>
2024-05-28 12:02:21 -0400

    Reformatted `Faq` using pylint

--- Cogs/Faq.py
+++ Cogs/Faq.py
@@ -14,7 +14,7 @@ async def setup(bot):
     
     # if file exists, appends channel names to a list for use with faq command
     if path.is_file():
-        with path.open() as f:
+        with path.open(encoding='utf-8') as f:
             for channel_name in f:
                 channel_name = channel_name.strip()
                 channel_names.append(channel_name)
@@ -85,6 +85,6 @@ class Faq(commands.Cog):
 
         channels_path = r"assets/FAQ/channels.txt"
         path = Path(channels_path)
-        with path.open('w') as f:
+        with path.open('w', encoding='utf=8') as f:
             for channel_name in self.channel_names:
                 f.write(f"{channel_name}\n")
