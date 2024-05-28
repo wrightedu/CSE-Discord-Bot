ecbec5e0347ade2741e075b3f737b33263566d3d
Makaela Sellers <sellers.52@wright.edu>
2024-05-28 14:36:25 -0400

    fixed a few more things

--- Cogs/CourseManagement.py
+++ Cogs/CourseManagement.py
@@ -12,10 +12,22 @@ from utils.utils import *
 
 
 async def setup(bot:commands.Bot):
+    """
+    Setup function to initialize the CourseManagement cog.
+
+    Parameters:
+        bot (commands.Bot): The bot instance.
+    """
     await bot.add_cog(CourseManagement(bot))
 
 
 class CourseManagement(commands.Cog):
+    """
+    A class representing commands for managing courses.
+
+    Parameters:
+        bot (commands.Bot): The bot instance.
+    """
     def __init__(self, bot):
         self.bot = bot
 
