902fcf8540b658577324f5b2c5b0e9010897c079
Makaela Sellers <sellers.52@wright.edu>
2024-05-30 13:48:31 -0400

    Re-wrote the logs using https://discordpy.readthedocs.io/en/latest/logging.html. Needs a lot more work.

--- bot.py
+++ bot.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python
 import os
 from time import time
+import logging
+import logging.handlers
+from dotenv import load_dotenv
 
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-from utils.utils import *
+# Configure logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
 
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf=8',
+    maxBytes=32 * 1024 * 1024, # 32 MiB
+    backupCount=5, # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Also output to console
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+# Initialize bot
 intents = discord.Intents(messages=True, guilds=True, members=True, voice_states=True, message_content=True)
-
-
 bot = commands.Bot(command_prefix='-', intents=intents)
 start_time = time()
 load_dotenv()
@@ -19,32 +39,21 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 @bot.event
 async def on_ready():
-    """Initializes cogs on bot startup
+    """Initializes cogs on bot startup"""
 
-    Begins logging
-    Loads all cogs
-    Sets status
-    Finishes startup log
-    """
-
-    # Startup status
+    logger.info('Bot is starting up...')
     await bot.change_presence(activity=discord.Game('Booting'), status=discord.Status.dnd)
+    logger.info('Changed status to Booting.')
 
-    # Start logging
-    await log(bot, '\n\n\n\n\n', False)
-    await log(bot, '###################################')
-    await log(bot, '# BOT STARTING FROM FULL SHUTDOWN #')
-    await log(bot, '###################################')
-
-    # Load all cogs
-    await bot.change_presence(activity=discord.Game(f'Loading Cogs'), status=discord.Status.idle)
+    logger.info('Loading cogs...')
     for file in os.listdir('Cogs'):
         if not file.startswith('__') and file.endswith('.py'):
             try:
                 await bot.load_extension(f'Cogs.{file[:-3]}')
-                await log(bot, f'Loaded cog: {file[:-3]}')
+                logger.info("Loaded cog: %s", {file[:-3]})
             except commands.errors.NoEntryPointError:
-                pass
+                logger.warning("No entry point found for cog: %s", {file[:-3]})
+
 
     # Show the bot as online
     # If the bot had a status prior to shutting down, restore it
@@ -59,13 +68,8 @@ async def on_ready():
             contents = 'Raider Up!'
         
     await bot.change_presence(activity=discord.Game(contents), status=discord.Status.online)
-    await log(bot, 'Bot is online')
-
-    # Print startup duration
-    await log(bot, '#########################')
-    await log(bot, '# BOT STARTUP COMPLETED #')
-    await log(bot, '#########################\n')
-    await log(bot, f'Started in {round(time() - start_time, 1)} seconds')
+    logger.info("Bot status set to %s", {contents})
+    logger.info("Bot startup completed in %s seconds.", {round(time() - start_time, 1)})
 
 
 @bot.event
@@ -80,20 +84,17 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Missing required argument')
         await ctx.send_help()
-        await log(bot, f'{author} attempted to run `{message}` but failed because they were missing a required argument')
-
+        logger.warning("%s attempted to run `%s` but failed: Missing required argument", {author}, {message})
     elif isinstance(error, commands.MissingRole):
         await ctx.send('Missing role')
-        await log(bot, f'{author} attempted to run `{message}` but failed because they were missing a required role')
-
+        logger.warning("%s attempted to run `%s` but failed: Missing role", {author}, {message})
     elif isinstance(error, commands.CommandNotFound):
-        await log(bot, f'{author} attempted to run `{message}` but failed because the command was not found')
-
+        logger.warning("%s attempted to run `%s` but failed: Command not found", {author}, {message})
     else:
         await ctx.send(f'Unexpected error: {error}')
-        await log(bot, f'{author} attempted to run `{message}` but failed because of an unexpected error: {error}')
+        logger.error("%s attempted to run `%s` but failed: %s", {author}, {message}, {error})
 
 
 if __name__ == '__main__':
     # Run bot from key given by command line argument
-    bot.run(TOKEN)
+    bot.run(TOKEN, log_handler=None) # Suppress default logging
