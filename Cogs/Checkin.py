dc241ce65d81c2abe58f9a691a0c8969e3c8133b
JakeHamblin <jake@jakehamblin.com>
2024-05-30 12:57:03 -0400

    Updated time interval

--- Cogs/Checkin.py
+++ Cogs/Checkin.py
@@ -224,7 +224,7 @@ class Checkin(commands.Cog):
         if pomodoros is not None:
             for pomodoro in pomodoros:
                 time = str(await get_time_epoch())
-                if float(time) >= (float(pomodoro[3]) + (1*60)):
+                if float(time) >= (float(pomodoro[3]) + (20*60)):
                     user = self.bot.get_user(int(pomodoro[8]))
 
                     if user is not None:
