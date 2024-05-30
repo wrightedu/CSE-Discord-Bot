185d6b183046f03d94aef0a851eaa69faaea218c
Makaela Sellers <sellers.52@wright.edu>
2024-05-30 12:13:33 -0400

    I removed `/load_all` because I realize that when the bot starts up, it already loads all cogs. Whoops.

--- Cogs/CogManagement.py
+++ Cogs/CogManagement.py
@@ -32,38 +32,7 @@ class CogManagement(commands.Cog):
     def __init__(self, bot, choices):
         self.bot = bot
         self.choices = choices
-
-
-    @app_commands.command(description="Load all cogs")
-    @app_commands.default_permissions(administrator=True)
-    async def load_all(self, interaction:discord.Interaction):
-        """Load all cogs
-        Load all cogs from the 'Cogs' directory
-
-        Outputs:
-            Message user informing them that all cogs are being loaded,
-            and when the action is done.
-        """
-        await interaction.response.send_message('Loading all cogs...')
-
-        cogs_directory = 'Cogs'
-        for filename in os.listdir(cogs_directory):
-            if filename.endswith('.py'):
-                cog_name = filename[:-3] # Remove the .py extension
-                try:
-                    await self.bot.load_extension(f'Cogs.{cog_name}')
-                    await interaction.channel.send(f'Cog {cog_name} has been loaded')
-                except:
-                    await interaction.channel.send(f'Cog {cog_name} is already loaded')
-                    return
-                await interaction.channel.send(f'Cog {cog_name} has been loaded')
-                await log(self.bot, f'{interaction.user} loaded the {cog_name} cog.')
-            else:
-                    await interaction.response.send_message(f'Cog {cog_name} does not exist. Please be sure you spelled it correctly.')
-                    await log(self.bot, f'{interaction.user} attempted to reload the {cog_name} cog, but failed.')
-
-        await log(self.bot, f'{interaction.user} loaded all cogs.')
-
+        
 
     @app_commands.command(description="Load a specific cog")
     @app_commands.default_permissions(administrator=True)
